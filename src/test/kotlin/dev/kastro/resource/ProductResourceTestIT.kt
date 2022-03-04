8a76aa477b7c4d602f7ce8041845c641cd572d02
Victor <victor.araujo@grupoboticario.com.br>
2022-03-04 15:16:16 -0300

    Add test to find product by id

--- src/test/kotlin/dev/kastro/resource/ProductResourceTestIT.kt
+++ src/test/kotlin/dev/kastro/resource/ProductResourceTestIT.kt
@@ -1,8 +1,12 @@
 package dev.kastro.resource
 
+import dev.kastro.FindByIdServiceRequest
 import dev.kastro.ProductServiceRequest
 import dev.kastro.ProductsServiceGrpc.ProductsServiceBlockingStub
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -22,5 +26,33 @@ internal class ProductResourceTestIT(
 
         assertEquals(1, response.id)
         assertEquals("product name", response.name)
+    }
+
+    @Test
+    fun `when ProductsServiceGrpc findById method is call with valid id a success is returned`() {
+        val request = FindByIdServiceRequest.newBuilder()
+            .setId(1)
+            .build()
+
+        val response = productsServiceBlockingStub.findById(request)
+
+        assertEquals(1, response.id)
+        assertEquals("Product A", response.name)
+    }
+
+    @Test
+    fun `when ProductsServiceGrpc findById method is call with invalid id a ProductNotFoundException is returned`() {
+        val request = FindByIdServiceRequest.newBuilder()
+            .setId(10)
+            .build()
+
+        val description = "Product with ID ${request.id} not found"
+
+        val response = Assertions.assertThrows(StatusRuntimeException::class.java) {
+            productsServiceBlockingStub.findById(request)
+        }
+
+        assertEquals(Status.NOT_FOUND.code, response.status.code)
+        assertEquals(description, response.status.description)
     }
 }
--- src/test/kotlin/dev/kastro/service/impl/ProductServiceImplTest.kt
+++ src/test/kotlin/dev/kastro/service/impl/ProductServiceImplTest.kt
@@ -3,12 +3,14 @@ package dev.kastro.service.impl
 import dev.kastro.domain.Product
 import dev.kastro.dto.ProductReq
 import dev.kastro.exceptions.AlreadyExistsException
+import dev.kastro.exceptions.ProductNotFoundException
 import dev.kastro.repository.ProductRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
+import java.util.*
 
 internal class ProductServiceImplTest {
     private val productRepository = Mockito.mock(ProductRepository::class.java)
@@ -41,6 +43,29 @@ internal class ProductServiceImplTest {
 
         Assertions.assertThrowsExactly(AlreadyExistsException::class.java) {
             productService.create((productReq))
+        }
+    }
+
+    @Test
+    fun `when findById method is call with valid id a ProductRes is returned`() {
+        val productInput = 1L
+        val productOutput = Product(id = 1, name = "product name", price = 10.00, quantityInStock = 5)
+
+        `when`(productRepository.findById(productInput))
+            .thenReturn(Optional.of(productOutput))
+
+        val productRes = productService.findById(productInput)
+
+        assertEquals(productInput, productRes.id)
+        assertEquals(productOutput.name, productRes.name)
+    }
+
+    @Test
+    fun `when findById method is call with invalid id, throws ProductNotFoundException`() {
+        val id = 1L
+
+        Assertions.assertThrowsExactly(ProductNotFoundException::class.java) {
+            productService.findById(id)
         }
     }
 }
